32592939aa722f3987402a85ac02de629cdc8f6e
ruong32 <trongruong99@gmail.com>
2019-10-05 18:24:44 +0700

    add models

---
+++ sh/env.example.sh
@@ -0,0 +1,9 @@
+export DB_CONNECTION=***
+export DB_HOST=***
+export DB_PORT=***
+export DB_NAME=***
+export DB_USERNAME=***
+export DB_PASSWORD=***
+
+export APP_HOST=***
+export APP_PORT=***
---
+++ sh/env.sh
@@ -0,0 +1,9 @@
+export DB_CONNECTION=mongodb
+export DB_HOST=localhost
+export DB_PORT=27017
+export DB_NAME=webchat
+export DB_USERNAME=""
+export DB_PASSWORD=""
+
+export APP_HOST=localhost
+export APP_PORT=3299
